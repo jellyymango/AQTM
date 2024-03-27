003b6747eca6aa49b018771e9e1f248c7720f552
mannydwal209 <105015664+mannydwal209@users.noreply.github.com>
2024-03-27 13:36:45 -0700

    Create data.js

---
+++ assets/data.js
@@ -0,0 +1,74 @@
+const originalData = [
+    { timestamp: "2000-02-01T05:00:00.000Z", temperature: 25 },
+    { timestamp: "2000-02-02T05:00:00.000Z", temperature: 30.35 },
+    { timestamp: "2000-02-03T05:00:00.000Z", temperature: 15.84 },
+    { timestamp: "2000-02-04T05:00:00.000Z", temperature: 50.92 },
+    { timestamp: "2000-02-05T05:00:00.000Z", temperature: 20.8 },
+    { timestamp: "2000-02-06T05:00:00.000Z", temperature: 15.47 },
+    { timestamp: "2000-02-07T05:00:00.000Z", temperature: 100.47 },
+    { timestamp: "2000-02-08T05:00:00.000Z", temperature: 20.47 },
+    { timestamp: "2000-02-09T05:00:00.000Z", temperature: 150.47 },
+    { timestamp: "2000-02-10T05:00:00.000Z", temperature: 8.3 },
+    { timestamp: "2000-02-11T05:00:00.000Z", temperature: 10.47 },
+    { timestamp: "2000-02-12T05:00:00.000Z", temperature: 100.47 },
+    { timestamp: "2000-02-13T05:00:00.000Z", temperature: 20.47 },
+    { timestamp: "2000-02-14T05:00:00.000Z", temperature: 50.47 },
+    { timestamp: "2000-02-15T05:00:00.000Z", temperature: 60.47 },
+];
+
+const animatedData = [
+    { timestamp: "2000-02-01T05:00:00.000Z", temperature: 50 },
+    { timestamp: "2000-02-02T05:00:00.000Z", temperature: 40.0 },
+    { timestamp: "2000-02-03T05:00:00.000Z", temperature: 30.0 },
+    { timestamp: "2000-02-04T05:00:00.000Z", temperature: 40.0 },
+    { timestamp: "2000-02-05T05:00:00.000Z", temperature: 50.0 },
+    { timestamp: "2000-02-06T05:00:00.000Z", temperature: 100.98 },
+    { timestamp: "2000-02-07T05:00:00.000Z", temperature: 50.0 },
+    { timestamp: "2000-02-08T05:00:00.000Z", temperature: 20.0 },
+    { timestamp: "2000-02-09T05:00:00.000Z", temperature: 130.75 },
+    { timestamp: "2000-02-10T05:00:00.000Z", temperature: 40.0 },
+    { timestamp: "2000-02-11T05:00:00.000Z", temperature: 50.0 },
+    { timestamp: "2000-02-12T05:00:00.000Z", temperature: 90.98 },
+    { timestamp: "2000-02-13T05:00:00.000Z", temperature: 60.0 },
+    { timestamp: "2000-02-14T05:00:00.000Z", temperature: 25.0 },
+    { timestamp: "2000-02-15T05:00:00.000Z", temperature: 33.67 },
+];
+
+const animatedData2 = [
+    { timestamp: "2000-02-01T05:00:00.000Z", temperature: 25 },
+    { timestamp: "2000-02-02T05:00:00.000Z", temperature: 30.0 },
+    { timestamp: "2000-02-03T05:00:00.000Z", temperature: 40.0 },
+    { timestamp: "2000-02-04T05:00:00.000Z", temperature: 10.0 },
+    { timestamp: "2000-02-05T05:00:00.000Z", temperature: 10.0 },
+    { timestamp: "2000-02-06T05:00:00.000Z", temperature: 70.0 },
+    { timestamp: "2000-02-07T05:00:00.000Z", temperature: 130.11 },
+    { timestamp: "2000-02-08T05:00:00.000Z", temperature: 90.0 },
+    { timestamp: "2000-02-09T05:00:00.000Z", temperature: 10.0 },
+    { timestamp: "2000-02-10T05:00:00.000Z", temperature: 10.0 },
+    { timestamp: "2000-02-11T05:00:00.000Z", temperature: 10.0 },
+    { timestamp: "2000-02-12T05:00:00.000Z", temperature: 70.0 },
+    { timestamp: "2000-02-13T05:00:00.000Z", temperature: 110.11 },
+    { timestamp: "2000-02-14T05:00:00.000Z", temperature: 90.0 },
+    { timestamp: "2000-02-15T05:00:00.000Z", temperature: 10.96 },
+];
+
+const animatedData3 = [
+    { timestamp: "2000-02-01T05:00:00.000Z", temperature: 25 },
+    { timestamp: "2000-02-02T05:00:00.000Z", temperature: 60.0 },
+    { timestamp: "2000-02-03T05:00:00.000Z", temperature: 35.0 },
+    { timestamp: "2000-02-04T05:00:00.000Z", temperature: 90.0 },
+    { timestamp: "2000-02-05T05:00:00.000Z", temperature: 8.0 },
+    { timestamp: "2000-02-06T05:00:00.000Z", temperature: 1000.76 },
+    { timestamp: "2000-02-07T05:00:00.000Z", temperature: 300.0 },
+    { timestamp: "2000-02-08T05:00:00:00.000Z", temperature: 100.0 },
+    { timestamp: "2000-02-09T05:00:00.000Z", temperature: 900.0 },
+    { timestamp: "2000-02-10T05:00:00.000Z", temperature: 900.0 },
+    { timestamp: "2000-02-11T05:00:00.000Z", temperature: 80.0 },
+    { timestamp: "2000-02-12T05:00:00.000Z", temperature: 1000.76 },
+    { timestamp: "2000-02-13T05:00:00.000Z", temperature: 300.0 },
+    { timestamp: "2000-02-14T05:00:00.000Z", temperature: 100.0 },
+    { timestamp: "2000-02-15T05:00:00.000Z", temperature: 900.53 },
+];
+
+export { originalData, animatedData, animatedData2, animatedData3 };
+
