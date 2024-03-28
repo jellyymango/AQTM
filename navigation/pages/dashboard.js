54f80ff6210dbb6d231c6e2ca22615308259899e
jellyymango <keo.joey@gmail.com>
2024-03-27 23:31:20 -0700

    updated dashboard.js
    added primitive temperature and time containers (using fake placeholder data)

--- navigation/pages/dashboard.js
+++ navigation/pages/dashboard.js
@@ -1,12 +1,93 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, useState } from 'react-native';
 
 export default function Dashboard({ navigation }) {
+    const weatherData = "78";
+    const timeData = "11:59";
+
+    return (
+ 
+        <View style={styles.weatherContainer}>
+     
+          <View style={styles.headerContainer}>
+     
+            <Text style={styles.tempText}>
+     
+              {weatherData}˚
+     
+            </Text>
+     
+          </View>
+     
+          <View
+     
+            style={{
+     
+              flex: 1,
+     
+              justifyContent: "flex-end",
+                
+              alignItems: "center",
+              //marginLeft: 30,
+     
+              marginBottom: 100,
+     
+            }}
+     
+          >
+     
+            <Text style={{ fontSize: 50, color: "black" }}>
+     
+              {timeData}
+
+     
+            </Text>
+     
+          </View>
+     
+        </View>
+     
+      );
+    /*
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text style={{ fontSize: 26, fontWeight: 'bold' }}>
                 Dashboard
             </Text>
         </View>
-    );
+    );*/
 }
+
+const styles = StyleSheet.create({
+ 
+  weatherContainer: {
+ 
+    flex: 1,
+ 
+    backgroundColor: "white",
+
+    textAlign: "center",
+ 
+  },
+ 
+  headerContainer: {
+ 
+    flexDirection: "row",
+ 
+    marginTop: 250,
+ 
+    justifyContent: "space-around",
+
+    display: "inline-block",
+ 
+  },
+ 
+  tempText: {
+ 
+    fontSize: 72,
+ 
+    color: "#000000",
+ 
+  },
+ 
+});
