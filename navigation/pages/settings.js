9166096a46f5fbeb87ce2df2ac38444d080633e9
JanPayoyo <andrewpayoyo@gmail.com>
2024-05-03 19:36:20 -0700

    Removed redundancy
    Moved threshold settings from settings page to the alerts page

--- navigation/pages/settings.js
+++ navigation/pages/settings.js
@@ -1,23 +1,15 @@
 import React, { useState } from 'react';
-import { View, Text, Switch, TextInput, StyleSheet } from 'react-native';
+import { View, Text, Switch, StyleSheet } from 'react-native';
 
 export default function SettingsPage({ temperatureUnit, setTemperatureUnit }) {
-  // State variables for appearance and notification settings
   const [darkMode, setDarkMode] = useState(false);
   const [notificationsEnabled, setNotificationsEnabled] = useState(true);
-
-  // State variables for threshold settings
-  const [temperatureThreshold, setTemperatureThreshold] = useState('');
-  const [formaldehydeThreshold, setFormaldehydeThreshold] = useState('');
-  const [noxThreshold, setNOXThreshold] = useState('');
-  const [vocThreshold, setVOCTreshold] = useState('');
-  const [humidityThreshold, setHumidityThreshold] = useState('');
-  const [pressureThreshold, setPressureThreshold] = useState('');
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Settings</Text>
-      {/* Display appearance settings */}
+      
+      {/* Appearance Settings */}
       <View style={styles.section}>
         <Text style={styles.sectionTitle}>Appearance</Text>
         <View style={styles.setting}>
@@ -26,7 +18,7 @@ export default function SettingsPage({ temperatureUnit, setTemperatureUnit }) {
         </View>
 
         <View style={styles.setting}>
-        <Text>Temperature Unit:</Text>
+          <Text>Temperature Unit:</Text>
           <View style={{ flexDirection: 'row', alignItems: 'center' }}>
             <Text>{temperatureUnit === 'Fahrenheit'? 'Fahrenheit' : 'Celsius'}</Text>
             <View style={{ width: 10 }} /> 
@@ -36,67 +28,15 @@ export default function SettingsPage({ temperatureUnit, setTemperatureUnit }) {
             />
           </View>
         </View>
-        
       </View>
-      {/* Display notification settings */}
+
+      {/* Notification Settings */}
       <View style={styles.section}>
         <Text style={styles.sectionTitle}>Notifications</Text>
         <View style={styles.setting}>
           <Text>Enable Notifications</Text>
           <Switch value={notificationsEnabled} onValueChange={setNotificationsEnabled} />
         </View>
-      </View>
-      {/* Display settings for temperature, formaldehyde, NOX, VOC, humidity, and pressure thresholds */}
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Threshold Settings</Text>
-        <Text style={styles.settingLabel}>Temperature Threshold:</Text>
-        <TextInput
-          style={styles.input}
-          value={temperatureThreshold}
-          onChangeText={setTemperatureThreshold}
-          keyboardType="numeric"
-          placeholder="Enter temperature threshold"
-        />
-        <Text style={styles.settingLabel}>Formaldehyde Threshold:</Text>
-        <TextInput
-          style={styles.input}
-          value={formaldehydeThreshold}
-          onChangeText={setFormaldehydeThreshold}
-          keyboardType="numeric"
-          placeholder="Enter formaldehyde threshold"
-        />
-        <Text style={styles.settingLabel}>NOX Threshold:</Text>
-        <TextInput
-          style={styles.input}
-          value={noxThreshold}
-          onChangeText={setNOXThreshold}
-          keyboardType="numeric"
-          placeholder="Enter NOX threshold"
-        />
-        <Text style={styles.settingLabel}>VOC Threshold:</Text>
-        <TextInput
-          style={styles.input}
-          value={vocThreshold}
-          onChangeText={setVOCTreshold}
-          keyboardType="numeric"
-          placeholder="Enter VOC threshold"
-        />
-        <Text style={styles.settingLabel}>Humidity Threshold:</Text>
-        <TextInput
-          style={styles.input}
-          value={humidityThreshold}
-          onChangeText={setHumidityThreshold}
-          keyboardType="numeric"
-          placeholder="Enter humidity threshold"
-        />
-        <Text style={styles.settingLabel}>Pressure Threshold:</Text>
-        <TextInput
-          style={styles.input}
-          value={pressureThreshold}
-          onChangeText={setPressureThreshold}
-          keyboardType="numeric"
-          placeholder="Enter pressure threshold"
-        />
       </View>
     </View>
   );
@@ -125,17 +65,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom: 10,
-  },
-  settingLabel: {
-    fontSize: 18,
-    marginBottom: 5,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
     marginBottom: 10,
   },
 });
